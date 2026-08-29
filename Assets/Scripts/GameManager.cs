d27a106bb3e53a95a8a22db210d58fb9569d4bd6
Darkvdz <famparintorn@gmail.com>
2026-08-30 02:02:19 +0700

    [code team] AddGameManager & timeOverSystem

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public Player player;
+
+    public float timeGame = 60f;
+    private float timeRemain;
+    private bool timeIsRun = false;
+    private bool gameHasStarted = false;
+
+    public TMP_Text timeText;
+    public GameObject winPanel;
+    public GameObject losePanel;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        timeRemain = timeGame;
+        timeText.text = "";
+    }
+
+    void Update()
+    {
+        if (timeIsRun)
+        {
+            if (timeRemain > 0)
+            {
+                timeRemain -= Time.deltaTime;
+                UpdateTimerUI();
+            }
+            else 
+            {
+                timeRemain = 0;
+                timeIsRun   = false;
+                UpdateTimerUI();
+                GameOver();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        if (gameHasStarted)
+        {
+            return;
+        }
+
+        gameHasStarted = true;
+        timeIsRun = true;
+
+        if (player != null)
+        {
+            player.StartGame();
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+      
+        int minutes = Mathf.FloorToInt(timeRemain / 60);
+        int seconds = Mathf.FloorToInt(timeRemain % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        if (timeRemain <= 10f && timeRemain > 0f)
+        {
+            float blink = Mathf.PingPong(Time.time * 5f, 1f);
+            timeText.color = Color.Lerp(Color.red, Color.white, blink);
+        }
+        else
+        {
+            timeText.color = Color.white;
+        }
+    }
+
+    public void GameOver()
+    {
+        timeIsRun = false;
+        losePanel.SetActive(true);
+        StopPlayer();
+        Time.timeScale = 0f;
+    }
+
+    public void GameWin()
+    {
+        if (!timeIsRun) 
+        { 
+            return; 
+        }
+        timeIsRun = false;
+        winPanel.SetActive(true);
+        StopPlayer();
+        Time.timeScale = 0f;
+    }
+
+    void StopPlayer()
+    { 
+        if (player != null)
+        {
+            player.isStart = false;
+            player.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+        }
+    }
+
+}
--- Assets/Scripts/StartBox.cs
+++ Assets/Scripts/StartBox.cs
@@ -2,18 +2,13 @@ using UnityEngine;
 
 public class StartBox : MonoBehaviour
 {
-    public Player player;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("check_1");
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (!(player.isStart))
-            {
-                print("check 2");
-                player.StartGame();
-            }
+            print("check_2");
+            GameManager.Instance.StartGame();
         }
     }
 }
